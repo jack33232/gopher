a91ed7abbe4ed4520ab85e7bbc104eed34f48032
jzhuo3 <jack33232@gmail.com>
2021-12-01 22:52:00 +0800

    - New Hello but breaking API

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,16 @@
 package hello
 
-import "fmt"
+import (
+	"net/http"
 
-func Hello() {
-	fmt.Println("Hello World from my gopher hello package.")
+	"github.com/gin-gonic/gin"
+)
+
+func Hello() *gin.Engine{
+	r := gin.Default()
+	r.NoRoute(func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World!")
+	})
+
+	return r
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,7 +1,20 @@
 package hello
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestHello(t *testing.T) {
-	Hello()
+	router := Hello()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "Hello World!", w.Body.String())
 }
